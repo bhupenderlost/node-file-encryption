6510ba731f7fb18b872da93459afb923b2381c2d
bhupenderlost <s.bhupender2401@gmail.com>
2022-09-10 10:42:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express   = require('express');
+const mongoose  = require('mongoose');
+
+//DB USERNAME AND PASSWORD
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+
+var DATABASE = process.env.DATABASE;
+DATABASE = DATABASE.replace("<user>", DB_USER);
+DATABASE = DATABASE.replace("<password>", DB_PASS);
+
+//PORT
+const PORT = process.env.PORT;
+
+//Init Express App
+const app = express();
+
+//Connect Database
+mongoose.connect(DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoIndex:true,
+    ignoreUndefined: true
+}).then(() => {
+    console.log('DB CONNECTED...')
+}).catch(err => console.log(err))
+
+//Use Public Folder As Content Serving
+app.use(express.static("public"));
+
+
+//Start The Server
+app.listen(PORT, () => {
+    console.log(`Server Running At PORT: ${PORT}`);
+});
